1e460f18e9d50d9167b4018b02c32f6703a80225
Jose Erik Ramos Dantas <jose.erikdantas@outlook.com>
2021-02-10 21:47:50 -0300

    Create Product Types View

---
+++ Dantas.Store.UI/Controllers/ProductTypesController.cs
@@ -0,0 +1,37 @@
+﻿using Dantas.Store.Data.EF.Repositories;
+using Dantas.Store.Domain.Contracts.Repositories;
+using Dantas.Store.Domain.Entities;
+using System.Web.Mvc;
+
+namespace Dantas.Store.UI.Controllers
+{
+    public class ProductTypesController : Controller
+    {
+        private readonly IRepositoryProductType _ProductTypeRepository = new RepositoryProductTypeEF();
+
+        public ViewResult Index()
+        {
+            var producttypes = _ProductTypeRepository.FindAll();
+
+            return View(producttypes);
+        }
+
+
+
+        [HttpGet]
+        public ViewResult AddNewType()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult AddNewType(ProductType prod)
+        {
+            var producttype = prod;
+            return View();
+        }
+       
+
+        
+    }
+}
